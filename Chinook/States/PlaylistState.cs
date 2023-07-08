d8c4b304d7050c560bfbc3f00b02e008be2d7c5e
ANgajasinghe <ag.nayanajith@gmail.com>
2023-07-08 19:30:51 +0530

    project complete

---
+++ Chinook/States/PlaylistState.cs
@@ -0,0 +1,27 @@
+﻿using Chinook.Models;
+
+namespace Chinook.States;
+
+public class PlaylistState
+{
+    public List<Playlist> Playlists { get; set; } = new();
+    public event Action? OnChange;
+    
+    public void SetPlaylists(List<Playlist> playlists)
+    {
+        Playlists = playlists;
+        OnChange?.Invoke();
+    }
+    
+    public void AddPlaylist(Playlist playlist)
+    {
+        Playlists.Add(playlist);
+        OnChange?.Invoke();
+    }
+    
+    public void RemovePlaylist(Playlist playlist)
+    {
+        Playlists.Remove(playlist);
+        OnChange?.Invoke();
+    }
+}
